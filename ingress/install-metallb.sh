0a119a003545b99376fdb1656b1447d9e55bcdc2
Algojo <36105680+meorkamil@users.noreply.github.com>
2026-05-14 05:03:37 +0800

    Adding traefik (#3)

--- ingress/install-metallb.sh
+++ ingress/install-metallb.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+
+# Install traefik
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+helm install traefik traefik/traefik \
+  --namespace traefik \
+  --create-namespace
